10569ef304cd145cf0e8f9bb57f9df0ca5e44b35
Amy <amyemoretti@gmail.com>
2019-01-31 16:31:31 -0600

    Just need to finish reload functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 const pikachuDiv = document.querySelector(".pikachu");
+const pikachuHP = document.querySelector(".pikachu-hp")
 const opponentDiv = document.querySelector(".opponent");
+const opponentHP = document.querySelector(".opponent-hp")
+
 const path = "assets/images/"
 
 const pokemonArray = [ 
@@ -37,20 +40,48 @@ const pokemonArray = [
     }
 ]
 
+
 random = pokemonArray[Math.floor(Math.random() * (pokemonArray.length -1))];
-opponentDiv.innerHTML = `<p>HP: ${random.hp}</p>`;
+opponentHP.innerHTML = `<p>HP: ${random.hp}</p>`;
 opponentImage = new Image();
 opponentImage.src = path + random.image;
 opponentImage.alt = random.name;
 opponentDiv.appendChild(opponentImage);
 
+console.log(random.hp);
+
 
 const pikachu = pokemonArray[3];    
-pikachuDiv.innerHTML = `<p>HP: ${pikachu.hp}</p>`;
+pikachuHP.innerHTML = `<p>HP: ${pikachu.hp}</p>`;
 const pikachuImage = new Image();
 pikachuImage.src = path + pikachu.image;
 pikachuImage.alt = pikachu.name;
 pikachuDiv.appendChild(pikachuImage);
 
+const pokemonDiv = document.querySelector(".pokemon");
+const attackButton = document.querySelector(".attack");
+const resetButton = document.querySelector(".reset");
+
+function attack(){
+    random.hp = (random.hp - pikachu.attack);
+    pikachu.hp = (pikachu.hp - random.attack);
+
+    pikachuHP.innerHTML = `<p>HP: ${pikachu.hp}</p>`;
+    opponentHP.innerHTML = `<p>HP: ${random.hp}</p>`;
+
+    checkStatus();
+
+}
+
+function checkStatus(){
+    if (pikachu.hp <= 0) {
+        pokemonDiv.innerHTML = "<p>You lose</p>";
+    } else if (random.hp <= 0) {
+        pokemonDiv.innerHTML = "<p>You win!</p>";
+    }
+}
+
+attackButton.addEventListener("click", attack);
+resetButton.addEventListener("click", document.reload);
 
 
